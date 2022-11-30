98a85c74368110bba488d94d7861223220903021
miracool45 <aokeosisi@gmail.com>
2022-11-30 15:46:51 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
-
+for j in range(97, 123):
+    print("{}".format(chr(j)), end=""
